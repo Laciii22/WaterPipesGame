0263c019242db42b1b0a756cc847bcd9349e0cac
Laciii22 <gardian.laci@gmail.com>
2023-04-23 18:11:24 +0200

    Final

--- src/main/java/sk/stuba/fei/uim/oop/Assignment2.java
+++ src/main/java/sk/stuba/fei/uim/oop/Assignment2.java
@@ -1,7 +1,6 @@
 package sk.stuba.fei.uim.oop;
 
 import sk.stuba.fei.uim.oop.gui.WaterPipesGame;
-
 public class Assignment2 {
     public static void main(String[] args) {
         new WaterPipesGame();
